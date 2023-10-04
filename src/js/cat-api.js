a0fcfc2dcbbac08b5c8fd76d06f7fca60ad1d4cb
azelenin057 <yazel057@gmail.com>
2023-10-04 23:15:01 +0300

    Final_nadeys

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,38 +1,23 @@
-import axios from 'axios';
-
 const url = 'https://api.thecatapi.com/v1';
 const api_key =
   'live_c3rFUokWMhzXC4qp5HYr4so0a73lviRVj9JyJbbjMGF6hwvN5ennXkFnIC69TWBF';
 
-// Функция для загрузки списка пород
-export async function fetchBreeds() {
-  try {
-    const response = await axios.get(`${url}/breeds`, {
-      headers: {
-        'x-api-key': api_key,
-      },
-    });
-    const breeds = response.data;
-    return breeds;
-  } catch (error) {
-    throw error;
-  }
+export function fetchBreeds() {
+  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
-// Функция для загрузки информации о коте по идентификатору породы
-export async function fetchCatByBreed(breedId) {
-  try {
-    const response = await axios.get(
-      `${url}/images/search?breed_ids=${breedId}`,
-      {
-        headers: {
-          'x-api-key': api_key,
-        },
-      }
-    );
-    const catData = response.data[0];
-    return catData;
-  } catch (error) {
-    throw error;
-  }
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,57 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js'; // Импорт функций из cat-api.js
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import '../css/styles.css';
 
-const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info');
+const refs = {
+  selector: document.querySelector('.breed-select'),
+  divCatInfo: document.querySelector('.cat-info'),
+  error: document.querySelector('.error'),
+  loader: document.querySelector('.loader'),
+};
 
-// Обработчик события загрузки страницы
-error.style.display = 'none';
-breedSelect.style.display = 'none';
+const { selector, divCatInfo, loader, error } = refs; //Деструктуризация рефсов
 
-window.addEventListener('load', () => {
-  fetchBreeds()
-    .then(breeds => {
-      // Обработка успешной загрузки списка пород
-      // Здесь можно наполнить select опциями
-      breedSelect.style.display = 'block';
-      loader.style.display = 'none';
+loader.classList.add('is-hidden');
+error.classList.add('is-hidden');
 
-      breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-      });
+fetchBreeds()
+  .then(res => {
+    const breedOptions = res.map(el => ({ text: el.name, value: el.id }));
+
+    breedOptions.forEach(optionData => {
+      const option = document.createElement('option');
+      option.value = optionData.value;
+      option.text = optionData.text;
+      selector.appendChild(option);
+    });
+  })
+  .catch(error => {
+    onFetchError();
+  });
+
+selector.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(event) {
+  loader.classList.remove('is-hidden');
+  selector.classList.add('is-hidden');
+  divCatInfo.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loader.classList.add('is-hidden');
+      selector.classList.remove('is-hidden');
+
+      const { url, breeds } = data[0];
+
+      divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+      divCatInfo.classList.remove('is-hidden');
     })
     .catch(error => {
-      // Обработка ошибки загрузки списка пород
-      showError();
-      loader.hidden = true;
-      error.hidden = false;
+      onFetchError();
     });
-});
+}
 
-// Обработчик события выбора породы
-breedSelect.addEventListener('change', () => {
-  const selectedBreedId = breedSelect.value;
-  loader.style.display = 'block'; // Показываем индикатор загрузки
-
-  fetchCatByBreed(selectedBreedId)
-    .then(catData => {
-      // Обработка успешной загрузки данных о коте
-      // Здесь можно отобразить информацию о коте
-      catInfo.innerHTML = `
-                <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
-                <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
-                <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
-                <img src="${catData.url}" alt="${catData.breeds[0].name}" width="400" />
-            `;
-    })
-    .catch(error => {
-      // Обработка ошибки загрузки данных о коте
-      showError();
-    })
-    .finally(() => {
-      loader.style.display = 'none'; // Скрываем индикатор загрузки после завершения запроса
-    });
-});
-
-// Функция для отображения сообщения об ошибке
-function showError() {
-  error.style.display = 'block';
+function onFetchError() {
+  selector.classList.add('is-hidden');
+  error.classList.remove('is-hidden');
 }
